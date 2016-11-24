fb03ba22ae935612fbdfdad2d393f0c3ee63e092
JunChaoWeb <214502706@qq.com>
2016-11-24 22:44:35 +0800

    tree select

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -3,6 +3,9 @@ namespace app\admin\controller;
 
 use think\Controller;
 use think\Session;
+use think\Db;
+use app\admin\model\Priv;
+
 
 class Index extends Base
 {
@@ -11,6 +14,30 @@ class Index extends Base
         return $this->fetch();
     }
     public function index(){
+        /*$priv = new Priv();
+        $list = $priv->where('priv_id','1')->find()->nodes;*/
+        /*foreach ($list as $key => $value) {
+            $list[$key]['']
+        }*/
+        //dump($list);
+        $list = Db::table('priv_node')
+                    ->alias('a')
+                    ->join('node b','a.node_id=b.node_id','LEFT')
+                    ->where('level',1)
+                    ->select();
+        foreach ($list as $key => $value) {
+            $id = $list[$key]['node_id'];
+            $list1 = Db::table('priv_node')
+                    ->alias('a')
+                    ->join('node b','a.node_id=b.node_id','LEFT')
+                    ->where('parent',$id)
+                    ->select();
+            $list[$key]['son'] = $list1;
+        }
+       
+        //$list = genTree($list);
+        dump($list);
+        $this->assign('list',$list);
         $uname = Session::get('uname');
     	return $this->fetch();
     }

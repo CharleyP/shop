5ecfba4e570ff274fb4d135163f78e4f2167cd5d
JunChaoWeb <214502706@qq.com>
2016-11-29 22:01:41 +0800

    角色模块关联操作

--- application/admin/model/Priv.php
+++ application/admin/model/Priv.php
@@ -1,6 +1,7 @@
 <?php 
 namespace app\admin\model;
 use think\Model;
+use think\Db;
 
 class Priv extends Model
 {
@@ -22,6 +23,107 @@ class Priv extends Model
     public function getNodes($id='')
     {
     	return $this->get($id)->nodes;
+    }
+    public function addPrivOne($data='')
+    {
+    	return $this->save($data);
+    }
+    public function privNodeAdd($privList='')
+    {
+    	$privName = $privList['roleName'];
+    	$remark = $privList['remark'];
+    	$addPrivData = array(
+    			'priv_name'	=>	$privName,
+    			'create_time'	=>	time(),
+    			'active'	=>	1,
+    			'remark'	=>	$remark,
+    		);
+    	$resultAdd = Db::table('priv')->insertGetId($addPrivData);
+    	if($resultAdd){
+    		$privId = $resultAdd;
+    	}else{
+    		return false;
+    		exit;
+    	}
+    	$privUpdata = array(
+				'priv_id'	=>	$privId,
+				'priv_name'	=>	$privName,
+				'remark'	=>	$remark,
+			);
+		unset($privList['roleName']);
+		unset($privList['remark']);
+		foreach ($privList as $key => $value) {
+			$addData[] = array(
+					'priv_id'	=>	$privId,
+					'node_id'	=>	$key,
+					'active'	=>	$value,
+				);
+		}
+		// 启动事务
+		Db::startTrans();
+		try{
+		    Db::table('priv_node')->where('priv_id',$privId)->delete();
+		    Db::table('priv_node')->insertAll($addData);
+		    // 提交事务
+		    Db::commit();    
+		    return true;
+		} catch (\Exception $e) {
+		    // 回滚事务
+		    Db::rollback();
+		    return false;
+		}
+    }
+    public function privNodeUpdate($privList='')
+    {
+    	/*$privId = $privList['privId'];
+    	$privName = $privList['roleName'];
+    	$remark = $privList['remark'];
+    	unset($privList['privId']);
+		unset($privList['roleName']);
+		unset($privList['remark']);
+		foreach ($privList as $key => $value) {
+			$nodeActives[] = array(
+					'active'	=>	$value,
+				);
+		}
+		$nodeIds = array_keys($privList);
+		$this->get($privId)->nodes()->detach($nodeIds);
+		$this->get($privId)->nodes()->saveAll($nodeIds,$nodeActives);*/
+		$privId = $privList['privId'];
+    	$privName = $privList['roleName'];
+    	$remark = $privList['remark'];
+    	$privUpdata = array(
+				'priv_id'	=>	$privId,
+				'priv_name'	=>	$privName,
+				'remark'	=>	$remark,
+			);
+    	unset($privList['privId']);
+		unset($privList['roleName']);
+		unset($privList['remark']);
+		foreach ($privList as $key => $value) {
+			$addData[] = array(
+					//'priv_id'	=>	intval($privId),
+					//'node_id'	=>	intval($key),
+					//'active'	=>	intval($value),
+					'priv_id'	=>	$privId,
+					'node_id'	=>	$key,
+					'active'	=>	$value,
+				);
+		}
+		// 启动事务
+		Db::startTrans();
+		try{
+			Db::table('priv')->update($privUpdata);
+		    Db::table('priv_node')->where('priv_id',$privId)->delete();
+		    Db::table('priv_node')->insertAll($addData);
+		    // 提交事务
+		    Db::commit();    
+		    return true;
+		} catch (\Exception $e) {
+		    // 回滚事务
+		    Db::rollback();
+		    return false;
+		}
     }
     /*public function privUpdate($id='',$arr)
     {	

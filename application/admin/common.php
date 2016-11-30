e0fe3964005461f91402ca98336f79b2a07e015a
JunChaoWeb <214502706@qq.com>
2016-11-30 22:10:38 +0800

    tree算法

--- application/admin/common.php
+++ application/admin/common.php
@@ -1,1 +1,23 @@
 <?php
+/**
+ * 递归获取树形结构算法
+ */
+function tree($items,$pid = 0){
+	$return = array();
+	foreach ($items as $item) {
+		if ($item['parent'] == $pid) {
+			$item['child'] = tree($items,$item['node_id']);
+			$return[] = $item;
+		}
+	}
+	return $return;
+}
+function ajax_return($data,$status,$msg){
+	$r = array(
+		'data'   =>$data,
+		'status' =>$status,
+		'msg'	 =>$msg
+		);
+	header('Content-Type:application/json;charset=utf-8');
+	exit(json_encode($r));
+}
